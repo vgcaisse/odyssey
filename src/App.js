710bb80393f6584a98e08339a7ba239f2675e240
Visnu Caisse <vgcaisse@gmail.com>
2023-06-01 18:48:46 -0400

    Quick Day Tracker Application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import "./App.css";
 
 function App() {
+  const [startDate, setStartDate] = useState("");
+  const [daysOutOfHome, setDaysOutOfHome] = useState("");
+  const [daysRemaining, setDaysRemaining] = useState(0);
+  const [error, setError] = useState("");
+  const [isDateSelected, setIsDateSelected] = useState(false);
+  const [isFormCompleted, setIsFormCompleted] = useState(false);
+
+  const handleCalculate = () => {
+    if (!isFormCompleted) {
+      return;
+    }
+
+    if (isNaN(daysOutOfHome)) {
+      setError("Are you coming back home today?!");
+      setDaysRemaining(0);
+      return;
+    }
+
+    if (daysOutOfHome < 0) {
+      setError("Time travelling not allowed!");
+      setDaysRemaining(0);
+      return;
+    }
+
+    const start = moment(startDate, "YYYY-MM-DD");
+    const current = moment();
+    const elapsedDays = current.diff(start, "days");
+    const remainingDays = Math.max(0, daysOutOfHome - elapsedDays);
+
+    if (remainingDays < 1) {
+      setError("Are you coming back home today?!");
+      setDaysRemaining(0);
+      return;
+    }
+
+    setError("");
+    setDaysRemaining(remainingDays);
+  };
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+
+    switch (id) {
+      case "start-date":
+        setStartDate(value);
+        setIsDateSelected(true);
+        break;
+      case "days-out":
+        if (isDateSelected) {
+          const intValue = parseInt(value);
+          setDaysOutOfHome(intValue >= 0 ? intValue : 0);
+        }
+        break;
+      default:
+        break;
+    }
+
+    setIsFormCompleted(isDateSelected && id === "days-out" && value !== "");
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      handleCalculate();
+    }, 1000 * 60 * 60 * 24); // Run the calculation once every 24 hours
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    handleCalculate();
+  }, [startDate, daysOutOfHome]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Place Tracker</h1>
+        <div className="form-group">
+          <label htmlFor="start-date">Start Date:</label>
+          <input
+            type="date"
+            id="start-date"
+            value={startDate}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="days-out">Days out of Home:</label>
+          <input
+            type="number"
+            id="days-out"
+            value={daysOutOfHome}
+            onChange={handleChange}
+            disabled={!isDateSelected}
+          />
+        </div>
+        {error && <div className="error">{error}</div>}
+        <div>
+          <p>Days Remaining: {isNaN(daysRemaining) ? null : daysRemaining}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,56 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Place Tracker/i);
+  expect(headerElement).toBeInTheDocument();
 });
+
+test('subtracts start date by days out of home', () => {
+  render(<App />);
+  const startDateInput = screen.getByLabelText(/Start Date:/i);
+  fireEvent.change(startDateInput, { target: { value: '2023-05-01' } });
+
+  const daysOutOfHomeInput = screen.getByLabelText(/Days out of Home:/i);
+  fireEvent.change(daysOutOfHomeInput, { target: { value: '5' } });
+
+  const daysRemainingElement = screen.getByText(/Days Remaining:/i);
+  expect(daysRemainingElement.textContent).toBe('Days Remaining: 0');
+});
+
+test('displays error message when daysRemaining is less than 1', () => {
+  render(<App />);
+  const startDateInput = screen.getByLabelText(/Start Date:/i);
+  fireEvent.change(startDateInput, { target: { value: '2023-05-20' } });
+
+  const daysOutOfHomeInput = screen.getByLabelText(/Days out of Home:/i);
+  fireEvent.change(daysOutOfHomeInput, { target: { value: '10' } });
+
+  const errorElement = screen.getByText(/Are you coming back home today\?/i);
+  expect(errorElement).toBeInTheDocument();
+});
+
+// these should be enough; I had a test that tested if a user unputed a string and if they tried inputing a negative value but I think I made it impossible for the user to achieve this so
+
+
+// just in case for future use:
+
+// test('throws error when user inputs a string value', () => {
+//   render(<App />);
+//   const daysOutOfHomeInput = screen.getByLabelText(/Days out of Home:/i);
+//   fireEvent.change(daysOutOfHomeInput, { target: { value: 'invalid' } });
+
+//   const errorElement = screen.getByText(/Please enter a valid number of days out of home./i);
+//   expect(errorElement).toBeInTheDocument();
+// });
+
+// test('throws error when user inputs a value lower than 0', () => {
+//   render(<App />);
+//   const daysOutOfHomeInput = screen.getByLabelText(/Days out of Home:/i);
+//   fireEvent.change(daysOutOfHomeInput, { target: { value: '-2' } });
+
+//   const errorElement = screen.getByText(/Time travelling not allowed!/i);
+//   expect(errorElement).toBeInTheDocument();
+// });
